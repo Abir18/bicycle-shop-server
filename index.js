3f5564b3514be989d282de383350a1896061c1f2
Abir18 <rifatabir21@gmail.com>
2021-11-15 07:11:17 +0600

    orders api created

--- index.js
+++ index.js
@@ -10,6 +10,45 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Mongodb Configaration
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8vsmo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log('database connected');
+
+    const database = client.db('bicycle_shop');
+    const bikesCollection = database.collection('bikes');
+    const ordersCollection = database.collection('orders');
+
+    // GET All Bikes
+    app.get('/bikes', async (req, res) => {
+      const cursor = bikesCollection.find({}).limit(10);
+      const result = await cursor.toArray();
+      res.json(result);
+    });
+
+    // POST Orders
+    app.post('/orders', async (req, res) => {
+      const order = req.body;
+      const result = await ordersCollection.insertOne(order);
+      res.json(result);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
   res.send('Bicycle Shop Server');
 });
